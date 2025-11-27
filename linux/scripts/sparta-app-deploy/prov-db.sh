9587c24350f6e073549498d7e9199c0fc2a20e54
Diana Culita <diana.culita100@gmail.com>
2025-11-27 10:30:27 +0000

    Database script idempotent test

--- linux/scripts/sparta-app-deploy/prov-db.sh
+++ linux/scripts/sparta-app-deploy/prov-db.sh
@@ -18,7 +18,7 @@ echo
 
 echo Install public gpg for MongoDB
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   sudo gpg --yes -o /usr/share/keyrings/mongodb-server-7.0.gpg \
    --dearmor
 echo Done!
 echo
